2686d35bf3eb5421631b76ec5fd1fdb2e76fd6ce
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-04-08 14:34:40 +1000

    further along

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,23 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        @article = Article.new(params[:article])
+        @article = Article.new(article_params)
 
         @article.save
         redirect_to @article
+    end
+
+def show
+    @article_params = Article.find(params[:id])
+end
+
+def new
+end
+
+private
+    def article_params
+        params.require(params[:article].permit(:title, :text))
+
     end
 
 end
